9c481133af631b81cd4ce24908f3847d511196e4
Joshua Todd <jmt879@student.bham.ac.uk>
2024-09-09 17:34:08 +0100

    added mnist zip and a way to extract for training

--- dataSets/mnist/mnist.go
+++ dataSets/mnist/mnist.go
@@ -1,15 +1,89 @@
 package mnist
 
 import (
+	"archive/zip"
 	"encoding/csv"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"gonum.org/v1/gonum/mat"
 )
 
+func Unzip(zipPath, destPath string) error {
+	r, err := zip.OpenReader(zipPath)
+	defer r.Close()
+
+	if err != nil {
+		return err
+	}
+
+	// Iterate through the files in the archive
+	for _, f := range r.File {
+
+		// Skip __MACOSX directory and its contents
+		if f.Name == "__MACOSX" || strings.HasPrefix(f.Name, "__MACOSX/") {
+			continue
+		}
+
+		// Create the full path for the file/directory to extract
+		fpath := filepath.Join(destPath, f.Name)
+
+		// Check if the file already exists
+		if _, err := os.Stat(fpath); err == nil {
+			continue
+		}
+
+		// Check if the entry is a directory or file
+		if f.FileInfo().IsDir() {
+			// Create the directory
+			os.MkdirAll(fpath, os.ModePerm)
+		} else {
+			// Ensure the directory exists for the file
+			if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+				return err
+			}
+
+			// Create the destination file
+			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer outFile.Close()
+
+			// Open the zip file entry for reading
+			rc, err := f.Open()
+			if err != nil {
+				return err
+			}
+
+			// Copy the contents of the file to the destination
+			_, err = io.Copy(outFile, rc)
+
+			rc.Close()
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
 func LoadMnistTrain() (*mat.Dense, *mat.Dense) {
+
+	err := Unzip("datasets/mnist/archive.zip", "datasets/mnist/")
+	if err != nil {
+		fmt.Println(err)
+	} else{
+    fmt.Println("Extracted mnist dataset")
+  }
+
 	var labels []float64
 	var features []float64
 
@@ -59,6 +133,8 @@ func LoadMnistTrain() (*mat.Dense, *mat.Dense) {
 
 	XTtrain := mat.NewDense(60000, 784, features)
 	yTrain := mat.NewDense(60000, 1, labels)
+
+  fmt.Println("Successfully read mnist dataset")
 
 	return XTtrain, yTrain
 }
